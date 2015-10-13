108626e24712ddf509f8a6e5ad6a8a0ea852fb15
kennpat <kennedypr@gmail.com>
2015-10-13 16:00:44 -0400

    Starting to get the javascript into the html pages for displaying different divs

--- spec/features/user_wants_to_add_a_new_companies_spec.rb
+++ spec/features/user_wants_to_add_a_new_companies_spec.rb
@@ -20,18 +20,33 @@ feature 'User adds a new corp structure', %Q{
   # This test will create the initial company entry within the database
   # subsequent tests will add subsidiaries to the structure.
   scenario 'User adds a company to their portfolio' do
-    visit new_corporation_path
+    visit root_path
+    click_on 'View Corporations'
+    click_on 'Add New Corporation'
     fill_in 'Corporation Name', with: 'Test Parent Company'
     check 'Is this a Parent Company?'
     click_on 'Add New Corporation'
     expect(page).to have_content('New Corporation created successfully')
   end
 
-  scenario 'User tries to add company without the proper information' do
-    visit new_corporation_path
-    click_on 'Add New Corporation'
-    expect(page).to have_content ("Corporation name can't be blank")
-  end
-
-
+  # scenario 'User tries to add company without the proper information' do
+  #   visit new_corporation_path
+  #   click_on 'Add New Corporation'
+  #   expect(page).to have_content ("Corporation name can't be blank")
+  # end
+  #
+  # scenario 'User wants to add a subsidiary to a parent company' do
+  #   #Create the parent company
+  #   visit new_corporation_path
+  #   fill_in 'Corporation Name', with: 'Test Parent Company'
+  #   check 'Is this a Parent Company'
+  #   click_on 'Add New Corporation'
+  #   #Create the subsidiary to attach to the parent company
+  #   visit new_corporation_path
+  #   fill_in 'Corporation Name', with: 'Test Subsidiary Company'
+  #   check 'Test Parent Company'
+  #   click_on 'Add New Corporation'
+  #   expect(page).to have_content('New Corporation created successfully')
+  #
+  # end
 end

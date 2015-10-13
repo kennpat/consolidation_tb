d140aae7ef1fc4f2282f1dbc358cf11ebb770e41
kennpat <kennedypr@gmail.com>
2015-10-13 17:05:21 -0400

    First test written for adding a trial balance, pre building out test

--- spec/features/user_wants_to_add_a_tb_to_a_corp_spec.rb
+++ spec/features/user_wants_to_add_a_tb_to_a_corp_spec.rb
@@ -13,10 +13,33 @@ feature 'User adds a new corp structure', %Q{
     fill_in 'Password', with: user.password
 
     click_button 'Log in'
-    # login_as(user)
+    @corporation = FactoryGirl.create(:corporation)
+
   end
 
   scenario 'User adds a trial balance to a corporation' do
+    visit corporation_path(@corporation.id)
+    click_on 'Add new financial period activity'
+    fill_in 'Cash', with: 100000
+    fill_in 'Accounts Receivable', with: 450000
+    fill_in 'Inventory', with: 1200000
+    fill_in 'Prepaid Expenses', with: 50000
+    fill_in 'Capital Assets', with: 1500000
+    fill_in 'Accumulated Depreciation', with: -1000000
+    fill_in 'Other Assets', with: 250000
+    fill_in 'Accounts Payable', with: -200000
+    fill_in 'Accrued Expenses', with: -350000
+    fill_in 'Long-term Liabilities', with: -450000
+    fill_in 'Other Liabilities', with: -100000
+    fill_in 'Retained Earnings', with: -1000000
+    fill_in 'Capital Stock', with: -400000
+    #note that the total of the entered amounts should be zero
+    click_on 'Submit'
+    expect(page).to have_content('New Trial Balance created successfully')
+
+  end
+
+  scenario 'User incorrectly adds a trial balance to a corporation' do
 
   end
 end
